aa3a7ba47addca5989105e77c5902d00b6d0a80a
MateiSR <matei2radu11@gmail.com>
2020-02-26 15:22:48 +0200

    Lambda Functions

---
+++ BasicFunctions/lambda_mod.py
@@ -0,0 +1,4 @@
+mod1 = lambda a : a % 10
+mod2 = lambda b : b % 100
+mod3 = lambda c : c % 1000
+print(mod1(10))
---
+++ BasicFunctions/sentence.py
@@ -0,0 +1,4 @@
+sentence = 'Lorem ipsum placerat in egestas erat imperdiet sed euismod est'
+words = sentence.split()
+print(words[7] + '; ' + words[8] + '; ' + words[9])
+
