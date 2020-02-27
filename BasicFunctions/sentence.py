b20c269feca85cfc7b335cc93b4cbe10c40eea96
Matei <matei2radu11@gmail.com>
2020-02-27 10:24:58 +0200

    Update sentence.py

--- BasicFunctions/sentence.py
+++ BasicFunctions/sentence.py
@@ -1,4 +1,6 @@
 sentence = 'Lorem ipsum placerat in egestas erat imperdiet sed euismod est'
 words = sentence.split()
-print(words[7] + '; ' + words[8] + '; ' + words[9])
+print('The last 3 words are: ' + words[7] + '; ' + words[8] + '; ' + words[9])
 
+sentence_new = sentence.replace(' sed euismod est', '') 
+print('The sentence without those last 3 words is: ' + sentence_new)
