37cdb19f5578537c46e1aff7f282be9886eb40a0
Matei Radu <matei2radu11@gmail.com>
2020-03-04 16:56:15 +0200

    update test encryption

--- ContactData/validate_contact.py
+++ ContactData/validate_contact.py
@@ -1,15 +1,16 @@
-# Validate phone no + mail & register in a file, salvat in variabila username + domain mail; 2 siruri de elemente, cate elemente din a < toate din b
 import os
 mail_valid = False
 phone_valid = False
 
 mail_address = ''
 phone_no = ''
+password = ''
 
 mail_valid1 = mail_address.find('@')
 mail_valid2 = mail_address.find('.com')
 
 current_dir = os.getcwd()
+current_dir = current_dir.replace('\\', '/')
 
 while mail_valid == False: 
     mail_valid1 = mail_address.find('@')
@@ -49,8 +50,8 @@ while valid_prefix == False:
             if phone_prefix[0] == list3[i][0] and phone_prefix[1] == list3[i][1] and phone_prefix[2] == list3[i][2]:
                 valid_prefix = True 
                 #print('ok 3')
-done = False
-while done == False:
+done_phone = False
+while done_phone == False:
     if valid_prefix == True:
         phone_no = input('Please enter your phone number (without the prefix): ')
         if len(phone_prefix) == 1:
@@ -58,7 +59,7 @@ while done == False:
 
                 print('Contact info OK (saving to file)')
 
-                done = True
+                done_phone = True
             elif len(phone_no) != 10:
                 print('Phone number should be 10 long.')
         if len(phone_prefix) == 2:
@@ -66,7 +67,7 @@ while done == False:
 
                 print('Contact info OK (saving to file)')
 
-                done = True
+                done_phone = True
             elif len(phone_no) != 9:
                 print('Phone number should be 9 long.')
         if len(phone_prefix) == 3:
@@ -74,23 +75,62 @@ while done == False:
 
                 print('Contact info OK (saving to file)')
 
-                done = True
+                done_phone = True
             elif len(phone_no) != 8:
                 print('Phone number should be 8 long.')
+
+
 
 domain = mail_address[mail_address.index('@') + 1:len(mail_address)]
 username = mail_address[0:mail_address.index('@')]
 
-if done == True:
+# convert to ascii and base 5
+
+password = ''
+password_ok = False
+
+while password_ok == False:
+    while len(password) < 8:
+        password = input('Input a password (min 8 chars): ')
+        password_list = []
+        for i in range(len(password)):
+            password_list.append(password[i])
+    if len(password) > 7:
+        password_ok = True
+
+def base_5(num):
+    done = False
+    num_reverse = ''
+    while done == False:
+        current_digit = num % 5
+        num = int(num / 5)
+        num_reverse += str(current_digit)
+        if num == 0:
+            done = True
+        global output
+        output = num_reverse[::-1]
+
+for j in range(len(password)):
+    password_list[j] = ord(password[j])
+
+for h in range(len(password)):
+    base_5(int(password_list[h]))
+    password_list[h] = output
+
+password_encrypted = '-'.join(password_list)
+
+#
+
+if done_phone == True and password_ok == True:
     if os.path.isdir(current_dir + '/output') != True:
         os.mkdir(current_dir + '/output')
         print('Creating directory \'/output/\'')
-    if os.path.isfile(current_dir + '/output/users.txt') != True:
-        os.mknod(current_dir + '/output/users.txt')
+    if os.path.isfile(current_dir + '/output/users.txt') == True:
+        os.remove(current_dir + '/output/users.txt')
         print('Creating file \'output/users.txt\'')
     try:
         f = open(current_dir + '/output/users.txt', 'w')
-        f.write('Username: \'' + username +'\'; Domain: \'' + domain + '\'; Phone: (' + str(phone_prefix) + ') ' + str(phone_no) + '\n')
+        f.write('Username: \'' + username +'\'; Domain: \'' + domain + '\'; Phone: (' + str(phone_prefix) + ') ' + str(phone_no) + '; Password: ' + str(password) + '; Password (Encrypted): ' + str(password_encrypted) + '\n')
         f.close()
     except OSError:
         print('Could not write to file')    
